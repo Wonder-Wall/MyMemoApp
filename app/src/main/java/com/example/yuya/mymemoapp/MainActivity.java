1d34bda4875c175cdfc0f6502e14a9b81214a73a
yuya <internous.yuya.hirose@gmail.com>
2015-07-24 10:05:52 +0900

    memoApp hirose

--- app/src/main/java/com/example/yuya/mymemoapp/MainActivity.java
+++ app/src/main/java/com/example/yuya/mymemoapp/MainActivity.java
@@ -1,17 +1,66 @@
 package com.example.yuya.mymemoapp;
 
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private SimpleCursorAdapter adapter;
+    public final static String EXTRA_MYID = "com.example.yuya.mymemoapp.MYID";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] from = {
+                MemoContract.Memos.COL_TITLE,
+                MemoContract.Memos.COL_UPDATED
+        };
+
+        int[] to = {
+                android.R.id.text1,
+                android.R.id.text2
+        };
+
+        adapter = new SimpleCursorAdapter(
+                this,
+                android.R.layout.simple_list_item_2,
+                null,
+                from,
+                to,
+                0
+        );
+
+        ListView myListView = (ListView) findViewById(R.id.myListView);
+        myListView.setAdapter(adapter);
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(
+                    AdapterView<?> parent,
+                    View view,
+                    int position,
+                    long id
+            ) {
+                Intent intent = new Intent(MainActivity.this, FormActivity.class);
+                intent.putExtra(EXTRA_MYID, id);
+                startActivity(intent);
+            }
+        });
+
+        getLoaderManager().initLoader(0, null, this);
     }
 
     @Override
@@ -23,10 +72,38 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_add) {
+            Intent intent = new Intent(this, FormActivity.class);
+            startActivity(intent);
             return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String[] projection = {
+                MemoContract.Memos._ID,
+                MemoContract.Memos.COL_TITLE,
+                MemoContract.Memos.COL_UPDATED
+        };
+        return new CursorLoader(
+                this,
+                MemoContentProvider.CONTENT_URI,
+                projection,
+                null,
+                null,
+                MemoContract.Memos.COL_UPDATED + " DESC"
+        );
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        adapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        adapter.swapCursor(null);
     }
 }
--- app/src/main/java/com/example/yuya/mymemoapp/MemoOpenHelper.java
+++ app/src/main/java/com/example/yuya/mymemoapp/MemoOpenHelper.java
@@ -5,42 +5,41 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 /**
- * Created by yuya on 2015/07/21.
+ * Created by yuya on 2015/05/24.
  */
 public class MemoOpenHelper extends SQLiteOpenHelper {
-    public static final String DB_NAME="myapp.db";
-    public static final int DB_VERSION= 1;
+    public static final String DB_NAME = "myapp.db";
+    public static final int DB_VERSION = 1;
 
     public static final String CREATE_TABLE =
-            "create table memos(" +
-                    "id integer primary key autoincrement, " +
+            "create table memos (" +
+                    "_id integer primary key autoincrement, " +
                     "title text, " +
                     "body text, " +
-                    "created date time default current_timestamp," +
-                    "updated date time default current_timestamp ";
+                    "created datetime default current_timestamp, " +
+                    "updated datetime default current_timestamp)";
     public static final String INIT_TABLE =
-            "insert into memos (title, body) values" +
-            "('t1','b1) ," +
-            "('t2','b2) ," +
-            "('t3','b3)";
+            "insert into memos (title, body) values " +
+                    "('t1', 'b1'), " +
+                    "('t2', 'b2'), " +
+                    "('t3', 'b3')";
     public static final String DROP_TABLE =
-            "drop table if exists memos" + MemoContract.Memos.TABLES_NAME;
+            "drop table if exists " + MemoContract.Memos.TABLE_NAME;
+
+
     public MemoOpenHelper(Context c) {
         super(c, DB_NAME, null, DB_VERSION);
     }
 
-
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(Create_TABLE);
+        db.execSQL(CREATE_TABLE);
         db.execSQL(INIT_TABLE);
-
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL(DROP_TABLE);
         onCreate(db);
-
     }
 }
